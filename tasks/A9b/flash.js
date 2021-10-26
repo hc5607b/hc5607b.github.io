ed50eb1a42b838a216d801d1546b69018e48a45c
hc5607b <hc5607b@gmail.com>
2021-10-26 11:14:05 +0300

    Added new task. need to finish this shit

---
+++ tasks/A9b/flash.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', onload);
+function onClick(){
+    while(1){
+        document.body.style.backgroundColor = "black";
+        // setTimeout('', 500);
+        // document.body.style.backgroundColor = "white";
+    }
+}
