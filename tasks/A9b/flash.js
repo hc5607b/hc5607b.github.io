e86074fe085eec049ab151abebfac5084c86fc19
hc5607b <hc5607b@gmail.com>
2021-10-26 12:57:30 +0300

    Added some js

--- tasks/A9b/flash.js
+++ tasks/A9b/flash.js
@@ -1,8 +1,43 @@
 window.addEventListener('load', onload);
+
+var counter = 0;
+var flashOn = 0;
+
+let colors = ["black", "white", "blue", "red", "yellow", "green", "pink", "lime", "cyan", "brown"];
+
 function onClick(){
-    while(1){
-        document.body.style.backgroundColor = "black";
-        // setTimeout('', 500);
-        // document.body.style.backgroundColor = "white";
+    if(flashOn == 0){flashOn = 1;}
+    else{flashOn = 0; returnColor();}
+    flash();
+}
+
+function flash(){
+    if(flashOn == 1){
+        counter++;
+    
+        if(counter < 1000){
+            if((counter % 2) == 1){
+                document.body.style.backgroundColor = colors[getRandomInt(colors.length)];
+            }else{
+                document.body.style.backgroundColor = colors[getRandomInt(colors.length)];
+            }
+            setTimeout(flash, 30);
+        }
+        else{counter = 0; returnColor();}
     }
+}
+
+function returnColor(){document.body.style.backgroundColor = "white";}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function jumpBtn(element){
+    var btn = document.getElementById('movingbtn');
+    btn.style.top = '100px';
+    // var btnEl = $(element);
+    // window.alert(btnEl);
+    // btnEl.style.backgroundColor = "black";
+    // $(element).insert(moveIt);
 }
